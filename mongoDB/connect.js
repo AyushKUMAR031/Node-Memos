322bb684f14e1f34347d31e32bd1e1dc8372b825
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2024-05-08 05:38:46 +0000

    update mongodb

--- mongoDB/connect.js
+++ mongoDB/connect.js
@@ -1,59 +1,60 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/trial')
-    .then(()=>{
-        console.log('connected to database');
+mongoose.connect("mongodb://localhost:27017/trial")
+    .then(() => {
+        console.log("connected to database");
     })
-    .catch(()=>{
-        console.log('connection failed');
-    })
+    .catch(() => {
+        console.log("connection failed");
+    });
 
 const userSchema = new mongoose.Schema({
     username: {
-      type: String,
-      required: true,
-      unique: true,
+        type: String,
+        required: true,
+        unique: true,
     },
     password: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
-  });
+});
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/',(req,res)=>{
-    // const data = fs.readFileSync('index.html');
-    // res.send(data);
-    res.sendFile(path.join(__dirname,'index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post('/submit',(req,res)=>{
+app.post("/submit", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
     const user = new User({
         username: username,
-        password: password
+        password: password,
     });
 
     user.save()
-    .then((user) => {
-        res.send('User registered successfully!');
-    })
-    .catch((err) => {
-        res.status(400).send('Unable to save to database');
-    });
-
+        .then((user) => {
+            res.send("User registered successfully!");
+        })
+        .catch((err) => {
+            res.status(400).send("Unable to save to database");
+        });
 });
 
-app.listen(8080);
+app.listen({
+    port: 8080
+},()=>{
+    console.log('Server started on http://localhost:3000');
+})
